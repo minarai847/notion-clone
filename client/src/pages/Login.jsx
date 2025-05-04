2835d82c4ed5a7cbc25092119ce70a008e83d1f5
岡庭　佑弥 <okaniwayuya@okaniwayuuwatarunoMacBook-Pro.local>
2025-05-04 21:54:34 +0900

    material icon install

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import AccessAlarmIcon from "@mui/icons-material/AccessAlarm"
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>Login
+    </div>
   )
 };
 
