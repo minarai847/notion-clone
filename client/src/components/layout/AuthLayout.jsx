1dc9979ad51e5141ac2d8bda03376295d851134c
minarai847 <guretodibaidinngu@yahoo.co.jp>
2025-05-05 20:08:53 +0900

    新規登録画面

--- client/src/components/layout/AuthLayout.jsx
+++ client/src/components/layout/AuthLayout.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { Container, Box } from '@mui/material';
+import notionLogo from "../../assets/images/notion-logo.png"
 
 const AuthLayout = () => {
   return (
     <div>AuthLayout
-      <Outlet />
+      <Container component="main" maxWidth="xs">
+        <Box sx={{
+          marginTop: 6,
+          diaplay: "flex",
+          flexDirection: "colunm"
+        }}>
+          <img src={notionLogo} alt=""
+            style={{ width: 100, height: 100, marginBottom: 3 }} />
+          Notionクローン開発
+
+        </Box>
+        <Outlet />
+      </Container>
     </div>
   )
 };
